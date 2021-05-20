125f4df7ef74b0f68622af295b4ce3102b61e22e
moT01 <20648924+moT01@users.noreply.github.com>
2021-05-20 16:57:14 -0500

    1404.1

--- .freeCodeCamp/reset_files/db.sql
+++ .freeCodeCamp/reset_files/db.sql
@@ -180,8 +180,6 @@ ALTER TABLE ONLY public.rentals ALTER COLUMN rental_id SET DEFAULT nextval('publ
 -- Data for Name: bikes; Type: TABLE DATA; Schema: public; Owner: freecodecamp
 --
 
-INSERT INTO public.bikes VALUES (1, 'Mountain', 27, true);
-INSERT INTO public.bikes VALUES (2, 'Mountain', 28, true);
 INSERT INTO public.bikes VALUES (3, 'Mountain', 29, true);
 INSERT INTO public.bikes VALUES (4, 'Road', 27, true);
 INSERT INTO public.bikes VALUES (5, 'Road', 28, true);
@@ -189,6 +187,8 @@ INSERT INTO public.bikes VALUES (6, 'Road', 29, true);
 INSERT INTO public.bikes VALUES (7, 'BMX', 19, true);
 INSERT INTO public.bikes VALUES (8, 'BMX', 20, true);
 INSERT INTO public.bikes VALUES (9, 'BMX', 21, true);
+INSERT INTO public.bikes VALUES (1, 'Mountain', 27, false);
+INSERT INTO public.bikes VALUES (2, 'Mountain', 28, false);
 
 
 --
@@ -201,6 +201,8 @@ INSERT INTO public.bikes VALUES (9, 'BMX', 21, true);
 -- Data for Name: rentals; Type: TABLE DATA; Schema: public; Owner: freecodecamp
 --
 
+INSERT INTO public.rentals VALUES (1, 1, 1, '2021-05-25', NULL);
+INSERT INTO public.rentals VALUES (2, 1, 2, '2021-05-25', NULL);
 
 
 --
@@ -221,7 +223,7 @@ SELECT pg_catalog.setval('public.customers_customer_id_seq', 1, false);
 -- Name: rentals_rental_id_seq; Type: SEQUENCE SET; Schema: public; Owner: freecodecamp
 --
 
-SELECT pg_catalog.setval('public.rentals_rental_id_seq', 1, false);
+SELECT pg_catalog.setval('public.rentals_rental_id_seq', 2, true);
 
 
 --
