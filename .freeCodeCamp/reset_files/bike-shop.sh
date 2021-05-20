f89ff9fe70f3e617e1f0088bd9b4cb90b9e1a930
moT01 <20648924+moT01@users.noreply.github.com>
2021-05-20 16:56:38 -0500

    1402.1

--- .freeCodeCamp/reset_files/bike-shop.sh
+++ .freeCodeCamp/reset_files/bike-shop.sh
@@ -84,6 +84,7 @@ RENT_MENU() {
         # set bike availability to false
 
         # get bike info
+        BIKE_INFO=$($PSQL "SELECT size, type FROM bikes WHERE bike_id = $BIKE_ID_TO_RENT")
 
         # send to main menu
 
