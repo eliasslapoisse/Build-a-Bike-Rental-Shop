fd58593f27259e0445ce1337bbb24e76e7be2187
Elias Imloul <elias.imloul@ensia.edu.dz>
2026-09-11 16:33:40 -0400

    Save progress: 1474.1

--- bike-shop.sh
+++ bike-shop.sh
@@ -100,7 +100,10 @@ RETURN_MENU() {
 	CUSTOMER_ID=$($PSQL "SELECT customer_id FROM customers WHERE phone='$PHONE_NUMBER';")
 
 	# if not found
-	# send to main menu
+	if [[ -z $CUSTOMER_ID ]]
+	then
+		# send to main menu
+	fi
 }
 
 EXIT() {
